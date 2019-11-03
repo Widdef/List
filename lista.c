ab1c5c095ee59a981e8ac0077b945a31e5211a6a
Maciej Żygowski <mzygowski@hotmail.com>
2019-11-03 22:23:36 +0100

    działa coś

--- lista.c
+++ lista.c
@@ -7,39 +7,37 @@ typedef struct ell
 	int data;
 	struct ell *next;
 
-}el_list;
-typedef el_list *list;
+}list;
 
-void list_add_end(list *p, int value)
+
+void list_add_end(list **p, int value)
 {
-	if (p == NULL)
-	{
-		list new_el = (list)malloc(sizeof(list));
-		new_el->data = value;
-		new_el->next = NULL;
-		p = new_el;
-	}
-	else
-	{
-		list pom = p->next;
-		list_add_end(pom, value);
+	list ** marker = p;
+	list *new_el = (list*)malloc(sizeof(list));
 
+	while (*marker)
+	{
+		marker = &(*marker)->next;
 	}
+	new_el->data = value;
+	new_el->next = NULL;
+	*marker = new_el;
 }
-void list_add_start(list *p, int value)
+
+void list_add_start(list **p, int value)
 {
 	if (p == NULL)
 	{
-		list new_el = (list)malloc(sizeof(list));
+		list *new_el = (list*)malloc(sizeof(list));
 		new_el->data = value;
 		new_el->next = NULL;
-		p = new_el;
+		*p = new_el;
 	}
 	else
 	{
-		list new_el = (list)malloc(sizeof(list));
+		list *new_el = (list*)malloc(sizeof(list));
 		new_el->data = value;
-		new_el->next = p;
+		new_el->next = *p;
 		*p = new_el;
 	}
 }
@@ -49,11 +47,11 @@ void list_reverse(list *p)
 	//list first;
 }
 
-void list_read_start(list p)
+void list_read_start(list *p)
 {
 	while (p != NULL)
 	{
-		printf("%djj\n", p->data);
+		printf("%d\n", p->data);
 		p = p->next;
 	}
 }
@@ -65,10 +63,10 @@ void list_read_end(list p)
 
 int main()
 {
-	list head = NULL;
-	list_add_end(head, 3);
-	//list_add_end(head, 5);
-	//list_add_start(head, 12);
+	list *head = NULL;
+	list_add_end(&head, 3);
+	list_add_end(&head, 5);
+	list_add_start(&head, 12);
 	list_read_start(head);
 
 	system("PAUSE");
